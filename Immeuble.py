8f316a372e5f4aed8df8817d800d8659972fad25
your Name <The name>
2026-09-11 20:30:33 +0200

    commit13

--- Immeuble.py
+++ Immeuble.py
@@ -9,12 +9,15 @@ def mega_giga_super_ultre_special_tera_kilo_peta_putain_ins():
     for i in range(4):
         tac = []
         for j in range(randint(1,5)):
+            tic = []
             if j == 0:
-                tac.append(randint(0,2))
+                tic.append(randint(0,2))
             else:
+                tic = []
                 for i in range(3):
-                    tac.append(randint(0,2))
-        ins[i].append(tac)
+                    tic.append(randint(0,2))
+            tac.append(tic)    
+        ins.append(tac)
     print(ins)             
     return None
 
@@ -36,26 +39,26 @@ def fenetre(x,y):
     carre(x,y,30,30,(255,255,255))
     return None
 
-def etage(x,y,c,p):
+def etage(x,y,c,ins,i,j):
     carre(x,y,140,60,c)
-    for i in range(3):
-        if p != i : fenetre(x+15+40*i,y+20)
+    for k in range(3):
+        if k not in ins[i][j] : fenetre(x+15+40*k,y+20)
         else:None
     return None
 
-def immeuble(x,y,c,ins):
-    for i in range(4):
-        etage(x,y+60*i,c,ins)
+def immeuble(x,y,c,ins,i):
+    for j in range(len(ins[i])):
+        etage(x,y+60*j,c,ins,i,j)
     return None
 
 def cartier(x,y,c,ins):
     for i in range(4):
-        immeuble(x+i*170,y,c[i],ins)
+        immeuble(x+i*170,y,c[i],ins,i)
     return None
 
 c = []
 for i in range(4):
     c.append((randint(1,255),randint(1,255),randint(1,255)))
-cartier(-200,-80,c,4)
+cartier(-200,-80,c,ins)
 
 done()
