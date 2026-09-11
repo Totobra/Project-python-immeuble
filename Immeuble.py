64f71ea9f272b5df2ebee2cc96b0a46eafc114e0
your Name <The name>
2026-09-11 13:23:04 +0200

    commit5

--- Immeuble.py
+++ Immeuble.py
@@ -1,16 +1,19 @@
 from turtle import *
 from random import *
 
-
-
+colormode(255)
 def carre(x,y,w,h,c):
-    goto(x,y)
+    teleport(x,y)
+    fillcolor(c)
+    begin_fill()
     for _ in range(2):
         forward(w)
         left(90)
         forward(h)
         left(90)
+    end_fill()    
     return None
 
+carre(20,40,30,-20,(12,68,240))
 
 done()
